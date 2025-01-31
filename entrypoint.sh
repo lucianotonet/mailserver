ab19358ea7e691d10356f28865c449f789344da2
Luciano T. <tonetlds@gmail.com>
2025-01-31 00:54:22 -0300

    fix: simplify configuration and remove manual hostname changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Set hostname and domainname
-hostnamectl set-hostname mail.tonet.dev || hostname mail.tonet.dev
-echo "mail.tonet.dev" > /etc/hostname
-echo "tonet.dev" > /etc/domainname
-
-# Update hosts file
-echo "127.0.0.1 mail.tonet.dev mail localhost localhost.localdomain" > /etc/hosts
-echo "::1 mail.tonet.dev mail localhost localhost.localdomain" >> /etc/hosts
-
 # Start the mailserver
 exec /usr/local/bin/start-mailserver.sh 
