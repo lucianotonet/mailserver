a6ca9e88ce94d64bcb0e2cbd5256b157b009378d
Luciano T. <tonetlds@gmail.com>
2025-01-31 00:56:11 -0300

    fix: adjust hostname configuration and add debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Debug information
+echo "Current hostname: $(hostname)"
+echo "Current domainname: $(hostname -d)"
+echo "Contents of /etc/hostname: $(cat /etc/hostname)"
+echo "Contents of /etc/hosts: $(cat /etc/hosts)"
+
 # Start the mailserver
 exec /usr/local/bin/start-mailserver.sh 
